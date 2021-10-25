4c68bfe786ecfef296e4fd5ef6b33ef921933a47
jpx <1074504362@qq.com>
2021-10-26 00:02:47 +0800

    文章页头部完成

--- src/components/Qa.vue
+++ src/components/Qa.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-      Qa
+    <div class="container">
+      <div>
+        <img src='@/assets/pictures/qa1.png'>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
 
-}
+  }
 </script>
 
 <style lang="less" scoped>
+  .container {
+    width: 1918px;
+    height: 383px;
+    background: #FFFFFF;
+    border-radius: 0px 0px 0px 0px;
+    opacity: 1;
+  }
 
+  .container img{
+    height: 100%;
+    width: 100%;
+  }
 </style>
