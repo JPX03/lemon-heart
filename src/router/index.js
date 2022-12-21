968d1b86c1df98d2b143d5ada2f07f725d8248a8
jiangoeixun <1074504362@qq.com>
2022-12-21 22:36:43 +0800

    node版本兼容

--- src/router/index.js
+++ src/router/index.js
@@ -1,116 +1,114 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Article from '@/components/ArticleCom/Article.vue'
-import ArticleDetai from '@/components/ArticleCom/ArticleDetais.vue'
-import More from '@/components/MoreCom/More.vue'
-import Login from '@/components/Login.vue'
-import Qa from '@/components/QaCom/Qa.vue'
-import Register from '@/components/Register.vue'
-import Test from '@/components/TestCom/Test.vue'
-import Home from '@/components/Home.vue'
-import QAndA from '@/components/QaCom/QAndA.vue'
-import PublishQ from '@/components/QaCom/PublishQ.vue'
-import UserPage from '@/components/UserCom/UserPage.vue'
-import Test1 from '@/components/TestCom/Test1.vue'
-import Test1Result from '@/components/TestCom/Test1Result.vue'
-import Test2 from '@/components/TestCom/Test2.vue'
-import Test3 from '@/components/TestCom/Test3.vue'
-import Test4 from '@/components/TestCom/Test4.vue'
-import Test5 from '@/components/TestCom/Test5.vue'
-import cookie from '@/utils/cookie.js'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Article from "@/components/ArticleCom/Article.vue";
+import ArticleDetai from "@/components/ArticleCom/ArticleDetais.vue";
+import More from "@/components/MoreCom/More.vue";
+import Login from "@/components/Login.vue";
+import Qa from "@/components/QaCom/Qa.vue";
+import Register from "@/components/Register.vue";
+import Test from "@/components/TestCom/Test.vue";
+import Home from "@/components/Home.vue";
+import QAndA from "@/components/QaCom/QAndA.vue";
+import PublishQ from "@/components/QaCom/PublishQ.vue";
+import UserPage from "@/components/UserCom/UserPage.vue";
+import Test1 from "@/components/TestCom/Test1.vue";
+import Test1Result from "@/components/TestCom/Test1Result.vue";
+import Test2 from "@/components/TestCom/Test2.vue";
+import Test3 from "@/components/TestCom/Test3.vue";
+import Test4 from "@/components/TestCom/Test4.vue";
+import Test5 from "@/components/TestCom/Test5.vue";
+import cookie from "@/utils/cookie.js";
 
-Vue.use(VueRouter)
-
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: [{
-      path: '/',
+  routes: [
+    {
+      path: "/",
       component: Home,
     },
     {
-      path: '/Home',
+      path: "/Home",
       component: Home,
     },
     {
-      path: '/Article',
+      path: "/Article",
       component: Article,
     },
     {
-      path: '/Article/:id',
+      path: "/Article/:id",
       component: ArticleDetai,
     },
     {
-      path: '/More',
+      path: "/More",
       component: More,
     },
     {
-      path: '/Qa',
+      path: "/Qa",
       component: Qa,
     },
     {
-      path: '/PublishQ',
+      path: "/PublishQ",
       component: PublishQ,
     },
     {
-      path: '/Qa/:id',
+      path: "/Qa/:id",
       component: QAndA,
     },
     {
-      path: '/Test',
+      path: "/Test",
       component: Test,
     },
     {
-      path: '/Test1',
+      path: "/Test1",
       component: Test1,
     },
     {
-      path: '/Test1Result',
+      path: "/Test1Result",
       component: Test1Result,
     },
     {
-      path: '/Test2',
+      path: "/Test2",
       component: Test2,
     },
     {
-      path: '/Test3',
+      path: "/Test3",
       component: Test3,
     },
     {
-      path: '/Test4',
+      path: "/Test4",
       component: Test4,
     },
     {
-      path: '/Test5',
+      path: "/Test5",
       component: Test5,
     },
     {
-      path: '/Login',
+      path: "/Login",
       component: Login,
     },
     {
-      path: '/Register',
+      path: "/Register",
       component: Register,
     },
     {
-      path: '/UserPage',
+      path: "/UserPage",
       component: UserPage,
-    }
+    },
   ],
-
 });
 
 // 路由守卫
 router.beforeEach(function (to, from, next) {
-
-  if (to.path == '/PublishQ' && !cookie.getCookie('userName')) {
-    alert('请先登录');
-  } else if (to.path == '/Test1' && !cookie.getCookie('userName')) {
-    alert('请先登录');
-  } else if (to.path == '/Test2' || to.path == '/Test3' || to.path == '/Test4' || to.path == '/Test5') {
-    alert('功能尚未开放，敬请期待噢！')
+  if (to.path == "/PublishQ" && !cookie.getCookie("userName")) {
+    alert("请先登录");
+  } else if (to.path == "/Test1" && !cookie.getCookie("userName")) {
+    alert("请先登录");
+  } else if (to.path == "/Test2" || to.path == "/Test3" || to.path == "/Test4" || to.path == "/Test5") {
+    alert("功能尚未开放，敬请期待噢！");
   } else {
     next();
   }
-})
+});
 
 export default router;
